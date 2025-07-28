65b539565d2d3bb6b807dc5568de93a8b5351f5d
Gokul <gokulram.n.dev@gmail.com>
2025-07-28 21:56:38 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base:"/Pittech/",
+  base:"/pittech/",
   // base: mode === "development" ? "/" : "/Pittech/",
   //base:"/",
   resolve: {
